f1dda6a5caad7050f55c5c49ca7be0d88969465b
lielb <liel-blu@campus.technion.ac.ilgit config --global user.email liel-blu@campus.technion.ac.il>
2022-05-19 14:31:00 +0300

    log in page

---
+++ app/src/main/java/com/example/medicheck/log_in_activity.kt
@@ -0,0 +1,29 @@
+package com.example.medicheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
+
+class log_in_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+    fun sign_in_button_clicked(view: View) {
+        val button = findViewById<Button>(R.id.sign_in_btn)
+        button.setOnClickListener {
+            val intent = Intent(this, appointments_activity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun sign_up_button_clicked(view: View) {
+        val button = findViewById<Button>(R.id.sign_up_btn)
+        button.setOnClickListener {
+            val intent = Intent(this, sign_up_activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/medicheck/sign_up_activity.kt
@@ -0,0 +1,11 @@
+package com.example.medicheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sign_up_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
